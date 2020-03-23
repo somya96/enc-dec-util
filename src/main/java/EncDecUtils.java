08ada863fba4d59641299e916ef2c1171053e297
Somya96 <sv1661@gmail.com>
2020-03-23 13:27:29 +0530

    setup project with gradle

---
+++ src/main/java/EncDecUtils.java
@@ -0,0 +1,26 @@
+
+public class EncDecUtils {
+    public void gradleTest() {
+        System.out.println("Successful");
+    }
+
+    public void encrypt(String fileContent) {
+        // BlockCipher engine = new DESEngine();
+        // BufferedBlockCipher cipher = new PaddedBlockCipher(new CBCCipher(engine));
+
+        // byte[] key = keyString.getBytes();
+        // byte[] input = fileContent.getBytes();
+
+        // cipher.init(true, new KeyParameter(key));
+
+        // byte[] cipherText = new byte[cipher.getOutputSize(input.length)];
+
+        // int outputLen = cipher.processBytes(input, 0, input.length, cipherText, 0);
+        // try {
+        // cipher.doFinal(cipherText, outputLen);
+        // } catch (CryptoException ce) {
+        // System.err.println(ce);
+        // System.exit(1);
+        // }
+    }
+}
